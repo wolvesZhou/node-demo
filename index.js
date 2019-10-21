33e07e4b03eaec244104feb5c8f7ce86f5c30c19
周炜烽 <zhouwf@dxy.cn>
2019-10-21 15:34:47 +0800

    chore: easy demo

---
+++ index.js
@@ -0,0 +1,23 @@
+const playerAction = process.argv(process.argv.length - 1);
+const number = Math.random() * 3;
+let computerAction;
+
+if (number < 1) {
+  computerAction = 'rock';
+} else if (number > 1) {
+  computerAction = 'scissor';
+} else {
+  computerAction = 'paper';
+}
+
+if (playerAction === computerAction) {
+  console.log('draw');
+} else if (
+  (playerAction === 'rock' && computerAction === 'scissor') ||
+  (playerAction === 'scissor' && computerAction === 'paper') ||
+  (playerAction === 'paper' && computerAction === 'rock')
+) {
+  console.log('You win')
+} else {
+  console.log('You lose')
+}
